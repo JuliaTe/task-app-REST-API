d593fe0fdb86233487d6b088e634a541dbcbf9cb
Julia Thea <yuliya.boyadjan@gmail.com>
2020-09-11 15:06:18 -0500

    completed show calls for users and tasks

--- src/index.js
+++ src/index.js
@@ -18,6 +18,18 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.get('/users', (req, res) => {
+  User.find({}).then((users) => {
+    res.send(users)
+  }).catch((e) => {
+    res.status(500).send()
+  })
+})
+
+app.get('', (req, res) => {
+  console.log(req.params)
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body)
 
@@ -25,6 +37,14 @@ app.post('/tasks', (req, res) => {
     res.status(201).send(task)
   }).catch((e) => {
     res.status(400).send(e)
+  })
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((tasks) => {
+    res.send(tasks)
+  }).catch((e) => {
+    res.status(500).send(e)
   })
 })
 
