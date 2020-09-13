26acf0c24b9f5497a73e2805fe1b8cffa32d3c03
Julia Thea <yuliya.boyadjan@gmail.com>
2020-09-12 20:20:31 -0500

    promise chaining practice

---
+++ playground/promise-chaining.js
@@ -0,0 +1,13 @@
+require('../src/db/mongoose')
+const User = require('../src/models/user')
+
+// 5f594ae148ef1898483682fc
+
+User.findByIdAndUpdate('5f594c6451315e98eef0015a', { age: 1 }).then((user) => {
+  console.log(user)
+  return User.countDocuments({ age: 1})
+}).then((result) => {
+  console.log(result)
+}).catch((e) => {
+  console.log(e)
+})
