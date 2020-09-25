9fe605164deb19b6d525a0090ec340be9109f0d6
Julia Thea <yuliya.boyadjan@gmail.com>
2020-09-24 19:13:19 -0500

    updated user model

--- src/models/user.js
+++ src/models/user.js
@@ -44,6 +44,8 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     const user = this
 
+    console.log('Just before saving')
+
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
