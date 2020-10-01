f64da484c0aa015c406f3155050a15b513ca992a
Julia Thea <yuliya.boyadjan@gmail.com>
2020-09-30 19:49:29 -0500

    updated user model to handle tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -42,6 +43,14 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+// methods are available on instances, called instance methods
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id }, 'thisnewtoken')
+    return token
+}
+
+// static methods are accessible on models, called model methods 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
