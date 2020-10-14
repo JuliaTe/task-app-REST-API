6b40582cbb071ba9aad16a75985c9c709406f67f
Julia Thea <yuliya.boyadjan@gmail.com>
2020-10-13 20:04:19 -0500

    refactored create task route to accept an owner field

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/tasks', async (req, res) => {
-  const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+  // const task = new Task(req.body)
+
+  const task = new Task({
+    ...req.body, // spread operator that copies all the properties from request body to this object
+    owner: req.user._id
+  })
+
   try {
     await task.save()
     res.status(201).send(task)
