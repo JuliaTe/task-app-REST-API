7f0a667bfc0fd61bbde667e0bbbb367ffb77688f
Julia Thea <yuliya.boyadjan@gmail.com>
2020-10-13 19:18:32 -0500

    some more delete user route refactoring

--- src/routers/user.js
+++ src/routers/user.js
@@ -105,13 +105,14 @@ router.patch('/users/:id', async (req, res) => {
 
 router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.user._id)
+        // const user = await User.findByIdAndDelete(req.user._id)
 
-        if (!user) {
-            return res.status(404).send()
-        }
+        // if (!user) {
+        //     return res.status(404).send()
+        // }
 
-        res.send(user)
+        await req.user.remove() // mongoose document
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
