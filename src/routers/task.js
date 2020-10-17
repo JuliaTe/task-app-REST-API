08bbdfbafdfe9a2bffa66c9b1b08ffec56176245
Julia Thea <yuliya.boyadjan@gmail.com>
2020-10-17 08:09:11 -0500

    added authentication to update task route

--- src/routers/task.js
+++ src/routers/task.js
@@ -46,7 +46,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
   }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'completed', 'description']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -57,18 +57,20 @@ router.patch('/tasks/:id', async (req, res) => {
 
   try {
     // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-  
-    const task = await Task.findById(req.params.id)
-    updates.forEach((update) => task[update] = req.body[update])
-    await task.save()
+    // const task = await Task.findById(req.params.id)
 
+    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+    
     // if (!isValidOperation) {
-    //   return res.status(400).send({ error: 'Not a valid update!' })
-    // }
+      //   return res.status(400).send({ error: 'Not a valid update!' })
+      // }
+      
+      if (!task) {
+        return res.status(404).send()
+      }
 
-    if (!task) {
-      return res.status(404).send()
-    }
+      updates.forEach((update) => task[update] = req.body[update])
+      await task.save()
 
     res.send(task)
 
