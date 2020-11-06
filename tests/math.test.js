cbffb8c94a8aaeb2b944cefc07fec365bd584af5
Julia Thea <yuliya.boyadjan@gmail.com>
2020-11-06 16:57:32 -0600

    another test case

--- tests/math.test.js
+++ tests/math.test.js
@@ -31,15 +31,15 @@ test('Should convert Celsius to Fahrenheit', () => {
 //   }, 2000)
 // })
 
-test('Should add two numbers', (done) => {
-  add(2, 3).then((sum) => {
-    expect(sum).toBe(5)
-    done()
-  })
-})
+// test('Should add two numbers', (done) => {
+//   add(2, 3).then((sum) => {
+//     expect(sum).toBe(5)
+//     done()
+//   })
+// })
 
-test('Should add two numbers async/await', async () => {
-  const sum = await add(10, 22)
-  expect(sum).toBe(32)
-})
+// test('Should add two numbers async/await', async () => {
+//   const sum = await add(10, 22)
+//   expect(sum).toBe(32)
+// })
 
