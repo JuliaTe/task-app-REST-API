2508454a2a9d0d4c4fc83fbb8dad7645f231cb8e
Julia Thea <yuliya.boyadjan@gmail.com>
2020-11-07 08:39:55 -0600

    test modification

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+  name: 'John Does',
+  email: 'johndoe@email.com',
+  password: 'somepass'
+}
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
 
 test('Should sign up a new user', async () => {
   await request(app).post('/users').send({
@@ -7,4 +19,8 @@ test('Should sign up a new user', async () => {
     'email': 'julezjs@email.com',
     'password': 'passwor11123'
   }).expect(201)
+})
+
+test('Should login existing user', async () => {
+  await request(app).post('/user/login')
 })
