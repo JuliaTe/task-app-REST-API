43d8603a52f9498181938c6d50d2dceb571887b7
Julia Thea <yuliya.boyadjan@gmail.com>
2020-11-09 10:13:57 -0600

    added a test for incorrect user login

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+// Creating a default user
 const userOne = {
    name: 'Mary Lou',
    email: 'marylou@email.com',
@@ -28,31 +29,9 @@ test('Should login existing user', async () => {
   }).expect(200)
 })
 
-
-
-// const userOne = {
-//     name: 'Mike',
-//     email: 'mike@example.com',
-//     password: '56what!!'
-// }
-
-// beforeEach(async () => {
-//     await User.deleteMany()
-//     await new User(userOne).save()
-// })
-
-// test('Should signup a new user', async () => {
-//     await request(app).post('/users').send({
-//         name: 'Andrew',
-//         email: 'andrew@example.com',
-//         password: 'MyPass777!'
-//     }).expect(201)
-// })
-
-// test('Should login existing user', async () => {
-//     await request(app).post('/users/login').send({
-//         email: userOne.email,
-//         password: userOne.password
-//     }).expect(200)
-// })
-
+test('Should not login a user with bad credentials', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: 'somepass123'
+  }).expect(400)
+})
