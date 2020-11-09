fa58d3706cf34e38b1090ee059bda3789d6e571e
Julia Thea <yuliya.boyadjan@gmail.com>
2020-11-09 10:29:31 -0600

    added test for authenticated user

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,19 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
 // Creating a default user
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+  _id: userOneId,
    name: 'Mary Lou',
    email: 'marylou@email.com',
-   password: '56what!!'
+   password: '56what!!',
+   tokens: [{
+     token: jwt.sign({ _id: userOneId }, process.env.USER_TOKEN)
+   }]
 }
 
 beforeEach(async () => {
@@ -34,4 +41,12 @@ test('Should not login a user with bad credentials', async () => {
     email: userOne.email,
     password: 'somepass123'
   }).expect(400)
+})
+
+test('Should get profile for user', async () => {
+  await request(app)
+  .get('/users/me')
+  .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+  .send()
+  .expect(200)
 })
